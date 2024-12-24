8d01977fb0b93080f50e7fe8d7261aab790870b8
AnastasiaYuhimenko <mail@yuhimenko-anastasia.ru>
2024-12-24 16:47:04 +0300

    pep8

--- microblog/microblog.py
+++ microblog/microblog.py
@@ -1,18 +1,22 @@
 from app import app
 from flask import render_template
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Настя'}
     posts = [
-        {'author': {'username': 'Vladik'}, 'body':'Iˆm a goose'},
+        {'author': {'username': 'Vladik'}, 'body': 'Iˆm a goose'},
         dict(author={'username': 'Goose'}, body='Iˆm a Vladik')
     ]
-    return render_template('index.html', title='Home',  user=user, posts=posts)
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
 @app.route('/test')
 def test():
     return 'test app'
+
 
 if __name__ == '__main__':
     app.run()
